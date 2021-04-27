eb2d0ab27f4e32980c7656e643d668dfacb4d39a
ShaoqiangLu <lushaoqiang27@gmail.com>
2021-04-28 00:24:50 +0800

    init

---
+++ src/leetcode/Solution2.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution2 {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> list = new ArrayList<>();
+        int begin = newInterval[0];
+        int end = newInterval[1];
+        for (int i = 0; i < intervals.length; i++) {
+            int left = intervals[i][0];
+            int right = intervals[i][1];
+            if (end < left) {
+                list.add(newInterval);
+            } else if (end < right) {
+                list.add(new int[] { Integer.min(begin, left), right });
+            } else if (end < intervals[i + 1][0]) {
+                if (begin <= right) {
+                    list.add(new int[] { Integer.min(begin, left), end });
+                } else {
+                    list.add(intervals[i]);
+                    list.add(newInterval);
+                }
+            } else {
+
+            }
+        }
+        int[][] result = new int[list.size()][];
+        return list.toArray(result);
+    }
+}
