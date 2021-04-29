0535931fae177a130f034a5db7fe581dd4b19125
ShaoqiangLu <lushaoqiang27@gmail.com>
2021-04-29 23:57:23 +0800

    21

---
+++ src/leetcode/Solution21.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class Solution21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode root = new ListNode();
+        ListNode pointer = root;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                pointer.next = l1;
+                l1 = l1.next;
+            } else {
+                pointer.next = l2;
+                l2 = l2.next;
+            }
+            pointer = pointer.next;
+        }
+        if (l1 != null) {
+            pointer.next = l1;
+        } else {
+            pointer.next = l2;
+        }
+        return root.next;
+    }
+}
