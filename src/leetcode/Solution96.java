9020b83de5b0b0f9df76fa7eaf118288a72d2653
ShaoqiangLu <lushaoqiang27@gmail.com>
2021-05-02 23:57:36 +0800

    96

---
+++ src/leetcode/Solution96.java
@@ -0,0 +1,18 @@
+public class Solution96 {
+    public int mySqrt(int x) {
+        int left = 0;
+        int right = x < 46340 ? x : 46340;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            int pow = mid * mid;
+            if (pow == x) {
+                return mid;
+            } else if (pow > x) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left - 1;
+    }
+}
