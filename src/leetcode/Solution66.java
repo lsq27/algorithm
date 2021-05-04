bc0a47bde79faf30583a62f5755f3847557e654a
ShaoqiangLu <lushaoqiang27@gmail.com>
2021-05-04 23:43:15 +0800

    66

---
+++ src/leetcode/Solution66.java
@@ -0,0 +1,15 @@
+public class Solution66 {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            digits[i]++;
+            if (digits[i] < 10) {
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
+}
---
+++ src/leetcode/Solution7.java
@@ -0,0 +1,14 @@
+public class Solution7 {
+
+    public int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            result = result * 10 + x % 10;
+            if (result != 0 && (result ^ x) < 0) {
+                return 0;
+            }
+            x /= 10;
+        }
+        return result;
+    }
+}
