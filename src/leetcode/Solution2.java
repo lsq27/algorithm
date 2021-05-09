1ab7276c4458a3c04a55a85dd75f0795d58c54bd
ShaoqiangLu <lushaoqiang27@gmail.com>
2021-05-09 23:52:45 +0800

    2

---
+++ src/leetcode/Solution2.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution2 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int idx = 0; idx < nums.length; idx++) {
+            int num = nums[idx];
+            int need = target - num;
+            if (map.containsKey(need)) {
+                return new int[] { map.get(need), idx };
+            } else {
+                map.put(num, idx);
+            }
+        }
+
+        return new int[0];
+    }
+}
