481ea27a9909f621d17bd2deed30317fa34f9a88
ShaoqiangLu <lushaoqiang27@gmail.com>
2021-05-17 23:56:26 +0800

    88

--- src/leetcode/Solution88.java
+++ src/leetcode/Solution88.java
@@ -2,31 +2,16 @@ package leetcode;
 
 public class Solution88 {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int point1 = 0;
-        int point2 = 0;
-        int extra = 0;
-        while (point1 < m && point2 < n) {
-            if (nums1[point1] <= nums2[point2] && nums1[point1] <= nums2[extra]) {
-                point1++;
-                continue;
+        int point1 = m - 1;
+        int point2 = n - 1;
+        int point = m + n - 1;
+        while (point1 >= 0 || point2 >= 0) {
+            if (point1 < 0 || (point2 >= 0 && nums1[point1] <= nums2[point2])) {
+                nums1[point--] = nums2[point2--];
             }
-            if (nums2[point2] <= nums1[point1] && nums2[point2] <= nums2[extra]) {
-                int tmp = nums1[point1];
-                nums1[point1] = nums2[point2];
-                nums2[point2] = tmp;
-                point1++;
-                point2++;
-                continue;
+            else {
+                nums1[point--] = nums1[point1--];
             }
-            if (nums2[extra] <= nums1[point1] && nums2[extra] <= nums2[point2]) {
-                int tmp = nums1[point1];
-                nums1[point1] = nums2[extra];
-                nums2[extra] = tmp;
-                point1++;
-            }
-        }
-        if (point1 == m) {
-
         }
     }
 }
