7b559c029d8e8a67bae51d6af86dc0a185114870
ShaoqiangLu <lushaoqiang27@gmail.com>
2021-05-21 23:56:47 +0800

    141

--- src/leetcode/Solution141.java
+++ src/leetcode/Solution141.java
@@ -1,7 +1,10 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution141 {
     public class Solution {
         public boolean hasCycle(ListNode head) {
-            Set<ListNode> seen = new HashSet<ListNode>();
+            Set<ListNode> seen = new HashSet<>();
             while (head != null) {
                 if (!seen.add(head)) {
                     return true;
