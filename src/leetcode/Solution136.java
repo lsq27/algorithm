8539d43555eef4962fc2932cdf2f19ba4175fa43
ShaoqiangLu <lushaoqiang27@gmail.com>
2021-05-26 22:30:45 +0800

    136

---
+++ src/leetcode/Solution136.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class Solution136 {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i : nums) {
+            result ^= i;
+        }
+        return result;
+    }
+}
