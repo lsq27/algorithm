f8e3e5da31d3af197ce55f5ba92d6d3f132a56a0
ShaoqiangLu <lushaoqiang27@gmail.com>
2021-05-31 23:31:23 +0800

    145

---
+++ src/leetcode/Solution145.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import common.TreeNode;
+
+public class Solution145 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        postorderTraversal(root, list);
+        return list;
+    }
+
+    public void postorderTraversal(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        postorderTraversal(root.left, list);
+        postorderTraversal(root.right, list);
+        list.add(root.val);
+    }
+}
