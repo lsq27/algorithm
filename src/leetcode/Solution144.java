2f555fa024caa7e7e550aa7097fd7f535ac92ef4
ShaoqiangLu <lushaoqiang27@gmail.com>
2021-05-31 23:08:04 +0800

    144

---
+++ src/leetcode/Solution144.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import common.TreeNode;
+
+public class Solution144 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        preorderTraversal(root, list);
+        return list;
+    }
+
+    public void preorderTraversal(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        list.add(root.val);
+        preorderTraversal(root.left, list);
+        preorderTraversal(root.right, list);
+    }
+}
