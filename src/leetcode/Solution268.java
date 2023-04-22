d03903b9f71c4dfce63027483cd6127248d11032
lsq27 <lushaoqiang27@gmail.com>
2023-04-22 23:19:24 +0800

    add 268

---
+++ src/leetcode/Solution268.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class Solution268 {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
