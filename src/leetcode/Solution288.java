8d2cc386d85d4f5cca8d00b65cfa953197ed8726
lsq27 <lushaoqiang27@gmail.com>
2023-05-08 23:49:12 +0800

    add 288

---
+++ src/leetcode/Solution288.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class Solution288 {
+    public void gameOfLife(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+            }
+        }
+    }
+}
