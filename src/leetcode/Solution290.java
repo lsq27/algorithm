fbe2673dab1ee8e11c4af89dd508c66d6e75bdf6
lsq27 <lushaoqiang27@gmail.com>
2023-05-15 23:50:35 +0800

    add 290

---
+++ src/leetcode/Solution290.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+import java.util.HashMap;
+
+public class Solution290 {
+    public boolean wordPattern(String pattern, String s) {
+        String[] strings = s.split(" ");
+        HashMap<Character, String> map = new HashMap<>();
+
+    }
+}
