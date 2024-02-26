56aa269a17d8388081bb64139aea8b28e5c9dc3b
Shaoqiang Lu <lushaoqiang27@gmail.com>
2024-02-26 23:36:25 +0800

    add 938

---
+++ src/leetcode/Solution938.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution938 {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        }
+        if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        }
+        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+    }
+
+    public int rangeSumBST2(TreeNode root, int low, int high) {
+        int sum = 0;
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if (node == null) {
+                continue;
+            }
+            if (node.val > high) {
+                q.offer(node.left);
+            } else if (node.val < low) {
+                q.offer(node.right);
+            } else {
+                sum += node.val;
+                q.offer(node.left);
+                q.offer(node.right);
+            }
+        }
+        return sum;
+    }
+}
